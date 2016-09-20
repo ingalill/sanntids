2444fda1d351ef24e14521998ea43bc3b92edf12
MalenB <malen_1995@hotmail.com>
2016-09-20 17:08:16 +0200

    Revert "Arduino kode"
    This reverts commit d8de6ea7f6b0cd32412179e24969ad9979462ecb.

--- Arduino/motor_control2/motor_control2.ino
+++ Arduino/motor_control2/motor_control2.ino
@@ -1,104 +1,85 @@
 //Standard DLL Speed control
-
+ 
 int E1 = 5;     //M1 Speed Control
 int E2 = 6;     //M2 Speed Control
 int M1 = 4;    //M1 Direction Control
 int M2 = 7;    //M1 Direction Control
-
+ 
 ///For previous Romeo, please use these pins.
 /*int E1 = 6;     //M1 Speed Control
-  int E2 = 9;     //M2 Speed Control
-  int M1 = 7;    //M1 Direction Control
-  int M2 = 8;    //M1 Direction Control*/
-
+int E2 = 9;     //M2 Speed Control
+int M1 = 7;    //M1 Direction Control
+int M2 = 8;    //M1 Direction Control*/
+ 
 void stop()                    //Stop
 {
-  digitalWrite(E1, LOW);
-  digitalWrite(E2, LOW);
-}
-void advance(char a, char b)         //Move forward
+  digitalWrite(E1,LOW);   
+  digitalWrite(E2,LOW);      
+}   
+void advance(char a,char b)          //Move forward
 {
-  analogWrite (E1, a);     //PWM Speed Control
-  digitalWrite(M1, HIGH);
-  analogWrite (E2, b);
-  digitalWrite(M2, LOW);
-}
-void back_off (char a, char b)         //Move backward
+  analogWrite (E1,a);      //PWM Speed Control
+  digitalWrite(M1,HIGH);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,LOW);
+}  
+void back_off (char a,char b)          //Move backward
 {
-  analogWrite (E1, a);
-  digitalWrite(M1, LOW);
-  analogWrite (E2, b);
-  digitalWrite(M2, HIGH);
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);   
+  analogWrite (E2,b);    
+  digitalWrite(M2,HIGH);
 }
-void turn_L (char a, char b)            //Turn Left
+void turn_L (char a,char b)             //Turn Left
 {
-  analogWrite (E1, a);
-  digitalWrite(M1, LOW);
-  analogWrite (E2, b);
-  digitalWrite(M2, LOW);
+  analogWrite (E1,a);
+  digitalWrite(M1,LOW);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,LOW);
 }
-void turn_R (char a, char b)            //Turn Right
+void turn_R (char a,char b)             //Turn Right
 {
-  analogWrite (E1, a);
-  digitalWrite(M1, HIGH);
-  analogWrite (E2, b);
-  digitalWrite(M2, HIGH);
+  analogWrite (E1,a);
+  digitalWrite(M1,HIGH);    
+  analogWrite (E2,b);    
+  digitalWrite(M2,HIGH);
 }
-void setup()
-{
+void setup() 
+{ 
   int i;
-  for (i = 4; i <= 7; i++)
-    pinMode(i, OUTPUT);
+  for(i=4;i<=7;i++)
+    pinMode(i, OUTPUT);  
   Serial.begin(19200);      //Set Baud Rate
   Serial.println("Run keyboard control");
-}
-int getSpeed ()            //Turn Right
+} 
+void loop() 
 {
-  int sp = 0;
-  if (Serial.available()) {
+  if(Serial.available()){
     char val = Serial.read();
-    Serial.println("Speed value:" + val);
-    if (val != -1) {
-      Serial.println(sp);
-    }
-  }
-  return sp;
-}
-void loop()
-{
-
-  if (Serial.available()) {
-    char val = Serial.read();
-    //int sp = 0;
-    if (val != -1)
+    if(val != -1)
     {
-
-      switch (val)
+      switch(val)
       {
-        case 'w'://Move Forward
-          /*while(!Serial.available() || (sp==0)){
-            sp=getSpeed();
-            }*/
-          Serial.println("Input:"+val);
-          advance (50, 50);  //move forward in max speed
-          break;
-        case 's'://Move Backward
-          back_off (50, 50);  //move back in max speed
-          break;
-        case 'a'://Turn Left
-          turn_L (50, 50);
-          break;
-        case 'd'://Turn Right
-          turn_R (50, 50);
-          break;
-        case 'z':
-          Serial.println("Hello");
-          break;
-        case 'x':
-          stop();
-          break;
+      case 'w'://Move Forward
+        advance (50,50);   //move forward in max speed
+        break;
+      case 's'://Move Backward
+        back_off (50,50);   //move back in max speed
+        break;
+      case 'a'://Turn Left
+        turn_L (50,50);
+        break;       
+      case 'd'://Turn Right
+        turn_R (50,50);
+        break;
+      case 'z':
+        Serial.println("Hello");
+        break;
+      case 'x':
+        stop();
+        break;
       }
     }
-    else stop();
+    else stop();  
   }
 }
