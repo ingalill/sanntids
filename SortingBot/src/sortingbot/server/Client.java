5caa0a30d2c94fab707313e6ee670b19336ca151
Inga Lill Bjølstad <ingabjolstad@gmail.com>
2016-10-19 09:49:51 +0200

    update på server/client

--- SortingBot/src/sortingbot/server/Client.java
+++ SortingBot/src/sortingbot/server/Client.java
@@ -9,17 +9,23 @@ package sortingbot.server;
  *
  * @author ingalillbjolstad
  */
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.*;
 import java.util.Date;
 //We need a Scanner to receive input from the user
 import java.util.Scanner;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
  
 public class Client{
     public static void main(String[] args) {
         new Client();
     }
-     
+    
+    // HA EN EGEN THREAD SOM TAR SEG AV MOTTAKELSEN AV FRAMES.
+        
+     // is going to ask for frames from the server
     public Client()
     {
     //We set up the scanner to receive user input
@@ -28,8 +34,8 @@ public class Client{
             
             Socket socket = new Socket("localhost",5000);
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             System.out.println("Client started at: " + new Date());
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));     
+            System.out.println("Client started at: " + new Date());
              
             //This will wait for the server to send the string to the client saying a connection
             //has been made.
@@ -42,9 +48,44 @@ public class Client{
                 String userInput = scanner.nextLine();
                 //Now we write it to the server
                 output.println(userInput);
+                System.out.println("Received " + userInput);
             }
         } catch (IOException exception) {
             System.out.println("Error: " + exception);
         }
     }
-}
+    
+     /*Convert an image to a mat
+     @Param BufferedImage input
+     @Return Mat output  
+    */
+    public static Mat imgToMat(BufferedImage in){
+        Mat out;
+        byte[] data;
+        int r,g,b;
+        
+        if(in.getType() == BufferedImage.TYPE_INT_RGB){
+            out = new Mat(240,320, CvType.CV_8UC3);
+            data = new byte[320*240 * (int)out.elemSize()];
+            int[] databuffer = in.getRGB(0,0,320,240,null,0,320);
+            for(int i=0; i<databuffer.length; i++){
+                data[i*3] = (byte) ((databuffer[i] >> 16) & 0xFF);
+                data[i*3+1] = (byte) ((databuffer[i]>> 8) & 0xFF);
+                data[i*3+2] = (byte) ((databuffer[i] >> 0) & 0xFF);
+            }         
+        }
+        else {
+            out = new Mat(240,320,CvType.CV_8UC3);
+            data = new byte[320*240 * (int)out.elemSize()];
+            int[] databuffer = in.getRGB(0,0,320,240,null,0,320);
+            for(int i=0; i<databuffer.length; i++){
+                r = (byte) ((databuffer[i] >> 16) & 0xFF);
+                g = (byte) ((databuffer[i] >> 8) & 0xFF);
+                b = (byte) ((databuffer[i] >> 0) & 0xFF);
+                data[i] = (byte) ((0.21 * r) + (0.71 *g) + (0.07 *b));
+            }
+        }
+        out.put(0,0,data);
+        return out;
+    }
+} // end of class
--- SortingBot/src/sortingbot/server/Server.java
+++ SortingBot/src/sortingbot/server/Server.java
@@ -1,53 +1,79 @@
-/*
- * http://hubpages.com/technology/How-to-create-a-server-in-Java 
-mulitthread: http://hubpages.com/technology/How-to-build-a-server-in-Java-Part-3-Allowing-multiple-users-to-connect 
- */
 package sortingbot.server;
 
+import java.awt.image.BufferedImage;
 import java.net.*;
 import java.util.*;
 import java.io.*;
+import sortingbot.VideoBox;
+import org.opencv.core.Mat;
 /**
  * @date 11.10.2016
  * @author inga lill bjolstad
  */
 public class Server {
+    private VideoBox videograbber;
     
     public static void main(String[] args){
-       // Server server =
-                new Server();
+        new Server();
     }
     
-     
     public Server() 
     {
         //We need a try-catch because lots of errors can be thrown
         try {
-            ServerSocket sSocket = new ServerSocket(5000);
+            ServerSocket serverSocket = new ServerSocket(5000);
             System.out.println("Server started at: " + new Date());
-             
-             System.out.println("Socket address:" + sSocket.getLocalSocketAddress());
             
             //Wait for a client to connect
-            Socket socket = sSocket.accept();
+            Socket socket = serverSocket.accept();
              
             //Create the streams
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             System.out.println("hei");
+            
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            
             //Tell the client that he/she has connected
             output.println("You have connected at: " + new Date());
-           
-             
+           //matToImg();
+            /*
+            Sende frames skal inn her, skal sendes hele tiden når connection er åpen
+            Sende størrelsen på bilde over socket
+            */
+            
             //Loop that runs server functions
             while(true) {
                 //This will wait until a line of text has been sent
                 String chatInput = input.readLine();
-                System.out.println(chatInput);
+                System.out.println("You got the input: " + chatInput);
             }
         } catch(IOException exception) {
             System.out.println("Error: " + exception);
         }
     }
-}
+    
+    /*
+    Skal i server
+    Take an Mat and convert it to an BufferedImage
+    @Param Mat input.
+    @Return BufferedImage output.
+    */
+    public static BufferedImage matToImg(Mat in){
+        BufferedImage out;
+        byte[] data = new byte[in.height() *in.width() *(int)in.elemSize()];
+        int type;
+        in.get(0,0,data);
+        
+        if(in.channels() == 0){
+         type = BufferedImage.TYPE_BYTE_GRAY;  
+        }
+        else{
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        out = new BufferedImage(in.height(),in.width(),type);
+        out.getRaster().setDataElements(0,0,in.height(),in.width(),data);
+        return out;
+    }
+    
+} // end of class
    
