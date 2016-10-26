992bf90c0febdda5d8efdc99fc2b9ebcb8248e20
MalenB <malen_1995@hotmail.com>
2016-10-26 11:40:34 +0200

    command box ting

--- SortingBot/src/sortingbot/CommandBox.java
+++ SortingBot/src/sortingbot/CommandBox.java
@@ -10,22 +10,23 @@ package sortingbot;
  * @author Demy
  */
 public class CommandBox {
-    boolean objectFound = false;
+    boolean objectFound;
     boolean objectFoundAvailable;
-    boolean autoDrive=true;
+    boolean autoDrive;
     boolean autoDriveAvailable;
     int adjustedDirection;
     boolean advance;
     boolean right;
     boolean left;
     boolean back;
+    boolean goalFound;
+    int state;
     
     // TODOOOOOOOOOOOO
     // -Vi treng noko som kan gir kommando at roboten skal stoppe (men kan fortsette der den slapp, så det blir ei slags pause funksjon)
     //- kommado for retning og fart
     //****************************************************************
     //- kommando for å ta imot et tall som tilsvarer både rettning og hastighet til roboten for når han kjører og skal til å svinge
-    //- Kommando for å ta imot distanse målt frå camera
     //- Kommando for å kjøre fremmover kalt advance
     //- Kommando for å kjøre til høre kalt right
     //- Kommando for å kjøre til venstre kalt left
@@ -34,9 +35,13 @@ public class CommandBox {
     
     
     public CommandBox(){
+        autoDrive=true;
+        objectFound = false;
         objectFoundAvailable=true;
         autoDriveAvailable=true;
         adjustedDirection=0;
+        goalFound=false;
+        state=0;
     }
     public synchronized void setObjectFound(boolean objectFound){
         while(!objectFoundAvailable){
@@ -73,5 +78,17 @@ public class CommandBox {
     }
    public int getAdjustedDirection(){
        return adjustedDirection;
+   }
+   public void setGoalFound(boolean aGoalFound){
+       goalFound=aGoalFound;
+   }
+   public boolean isGoalFound(){
+       return goalFound;
+   }
+   public void setState(int aState){
+       state=aState;
+   }
+   public int getState(){
+       return state;
    }
 }
