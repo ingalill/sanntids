a14df810de8d711361f5c3a0cfa0a5e26dc1621d
MalenB <malen_1995@hotmail.com>
2016-11-22 12:54:24 +0100

    exit

--- SortingBot/src/sortingbot/ArduinoCommunication.java
+++ SortingBot/src/sortingbot/ArduinoCommunication.java
@@ -72,6 +72,7 @@ public class ArduinoCommunication implements SerialPortEventListener {
 		}
 		if (portId == null) {
 			System.out.println("Could not find COM port.");
+                        System.exit(1);
 			return;
 		}
 
