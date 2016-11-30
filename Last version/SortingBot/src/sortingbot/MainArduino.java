3db655094802758a339757b7dd1451c6323618c8
MalenB <malen_1995@hotmail.com>
2016-11-30 15:03:20 +0100

    små fix

--- Last version/SortingBot/src/sortingbot/MainArduino.java
+++ Last version/SortingBot/src/sortingbot/MainArduino.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sortingbot;
 
 /**
- *
- * @author MalenB
+ * A class for testing the arduino part
+ * @author Malen
  */
 public class MainArduino {
     private static ArduinoCommunication communication;
